79a15ab6011a19f0d781d29d543ad8af7fba4e47
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2019-04-03 11:13:55 +0700

    update db

--- lib/mailerpln.sql
+++ lib/mailerpln.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 31, 2019 at 11:07 AM
+-- Generation Time: Apr 03, 2019 at 06:12 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -43,8 +43,9 @@ CREATE TABLE `tbl_login` (
 --
 
 INSERT INTO `tbl_login` (`id_login`, `username`, `password`, `email`, `email_pass`, `login_role`, `last_active`) VALUES
-(1, 'admin', '123456', 'codefive65@gmail.com', 'Pace1996', 0, '2019-03-31 06:05:41'),
-(2, 'mhdiriansyah', 'Pace1996', 'muhammadiriansyah@ralali.com', '', 1, '2019-03-25 09:43:55');
+(1, 'admin', '123456', 'codefive65@gmail.com', 'Pace1996', 0, '2019-04-02 18:13:04'),
+(2, 'mhdiriansyah', 'Pace1996', 'muhammadiriansyah@ralali.com', '', 1, '2019-04-03 03:56:58'),
+(3, 'thareqtito', 'tito2019', 'thareqtito@gmail.com', NULL, 2, '2019-04-03 04:08:30');
 
 -- --------------------------------------------------------
 
@@ -57,10 +58,13 @@ CREATE TABLE `tbl_vendor` (
   `no_kontrak` varchar(100) NOT NULL,
   `nama_vendor` text NOT NULL,
   `email_vendor` varchar(100) NOT NULL,
+  `email_ops1` varchar(100) NOT NULL,
+  `email_ops2` varchar(100) NOT NULL,
   `uraian` text NOT NULL,
   `tanggal_mulai` date NOT NULL,
   `tanggal_selesai` date NOT NULL,
   `kirim_notif` int(1) NOT NULL,
+  `kirim_notif_langsung` int(1) NOT NULL,
   `nilai_kontrak_spj_spk` int(12) DEFAULT NULL,
   `nilai_kontrak_akhir_amd` int(12) DEFAULT NULL,
   `penagihan_tanggal` date NOT NULL,
@@ -73,11 +77,8 @@ CREATE TABLE `tbl_vendor` (
 -- Dumping data for table `tbl_vendor`
 --
 
-INSERT INTO `tbl_vendor` (`id_vendor`, `no_kontrak`, `nama_vendor`, `email_vendor`, `uraian`, `tanggal_mulai`, `tanggal_selesai`, `kirim_notif`, `nilai_kontrak_spj_spk`, `nilai_kontrak_akhir_amd`, `penagihan_tanggal`, `penagihan_rupiah`, `pembayaran_rupiah`, `progress_pekerjaan`) VALUES
-(12, 'kontrak0001', 'CODE5', 'ryanjoker87@gmail.com', 'membangun jaringan internet', '2019-03-10', '2019-03-29', 1, 650000, 1150000, '2019-03-25', 14000000, 15000000, 88),
-(14, 'kontrak00002', 'TAKUPAZ', 'riantri271@gmail.com', 'wkwk', '2019-03-06', '2019-03-27', 1, 5000000, 6000000, '2019-03-25', 5000000, 5000500, 98),
-(15, 'kontrak0003', 'PT THREECRAFT', 'sri1531029@sttpln.ac.id', 'Banyak-banyak membaca', '2019-03-14', '2019-03-25', 1, 6000000, 6500000, '2019-03-25', 7500000, 8500000, 87),
-(16, 'kontrakan', 'Kontrakan Ber-5', 'thareqtito@gmail.com', 'Membahas mengenai keutamaan hati', '2019-03-14', '2019-04-16', 1, 5600000, 6000000, '2019-04-04', 6000000, 6500000, 67);
+INSERT INTO `tbl_vendor` (`id_vendor`, `no_kontrak`, `nama_vendor`, `email_vendor`, `email_ops1`, `email_ops2`, `uraian`, `tanggal_mulai`, `tanggal_selesai`, `kirim_notif`, `kirim_notif_langsung`, `nilai_kontrak_spj_spk`, `nilai_kontrak_akhir_amd`, `penagihan_tanggal`, `penagihan_rupiah`, `pembayaran_rupiah`, `progress_pekerjaan`) VALUES
+(1, 'kontrak0001', 'CODE5 FIVE Corp', 'muhammad.iriansyah@ralali.com', 'ryanjoker87@gmail.com', 'iriansyah1431299@sttpln.ac.id', 'Mengintalasi Service Worker', '2019-04-01', '2019-04-09', 1, 1, 5000000, 6000000, '2019-04-04', 8000000, 8500000, 89);
 
 --
 -- Indexes for dumped tables
@@ -103,13 +104,13 @@ ALTER TABLE `tbl_vendor`
 -- AUTO_INCREMENT for table `tbl_login`
 --
 ALTER TABLE `tbl_login`
-  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tbl_vendor`
 --
 ALTER TABLE `tbl_vendor`
-  MODIFY `id_vendor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_vendor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
